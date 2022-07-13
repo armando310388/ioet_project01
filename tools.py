6a51aa6d044ca95a8eae11bff90e7cbfb511f91b
Julio Rivera <julio.armando@bounteous.com>
2022-07-13 14:27:54 -0500

    Added calculate_amount logic

--- tools.py
+++ tools.py
@@ -13,6 +13,24 @@ def convert_data_day_to_list(single_data: str) -> list:
     pass
 
 
-def calculate_amount(single_matrix: list) -> int:
+def calculate_amount(data: dict) -> int:
     """Method for calculating the amount to pay for a employee"""
-    pass
+    payment_per_hour = {
+        'MO': [25, 25, 25, 25, 25, 25, 25, 25, 25, 15, 15, 15, 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20, 20],
+        'TU': [25, 25, 25, 25, 25, 25, 25, 25, 25, 15, 15, 15, 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20, 20],
+        'WE': [25, 25, 25, 25, 25, 25, 25, 25, 25, 15, 15, 15, 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20, 20],
+        'TH': [25, 25, 25, 25, 25, 25, 25, 25, 25, 15, 15, 15, 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20, 20],
+        'FR': [25, 25, 25, 25, 25, 25, 25, 25, 25, 15, 15, 15, 15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20, 20],
+        'SA': [30, 30, 30, 30, 30, 30, 30, 30, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25],
+        'SU': [30, 30, 30, 30, 30, 30, 30, 30, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 25, 25, 25, 25, 25, 25]
+    }
+
+    total_amount = 0
+
+    for day in data:
+        for i, j in zip(data[day], payment_per_hour[day]):
+            total_amount += i*j
+
+    return total_amount
+
+
