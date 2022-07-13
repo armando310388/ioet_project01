388ec30d51bcfd26e146341d3c1f6bd59183b20d
Julio Rivera <julio.armando@bounteous.com>
2022-07-13 16:48:24 -0500

    Added convert_data_day_to_list logic

--- tools.py
+++ tools.py
@@ -3,14 +3,16 @@ def validate_data(single_data: str) -> bool:
     pass
 
 
-def extract_data_day(single_data:str) -> list:
+def extract_data_day(single_data: str) -> list:
     """Method for extracting each single day data from a single data from a employee"""
     pass
 
 
 def convert_data_day_to_list(single_data: str) -> list:
     """Method for converting a single day data from a employee to a list based on hours on a single day"""
-    pass
+    initial_hour = int(single_data[2:4])
+    end_hour = int(single_data[8:10])
+    return [1 if initial_hour <= i < end_hour else 0 for i in range(24)]
 
 
 def calculate_amount(data: dict) -> int:
